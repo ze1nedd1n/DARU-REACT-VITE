52905b50ac40a98f282769a2791e03e614aaf241
Zeineddin <101195142+ze1nedd1n@users.noreply.github.com>
2024-02-18 04:27:46 +0600

    задачи за 31 января сделаны

--- daru-winter-vite/src/App.tsx
+++ daru-winter-vite/src/App.tsx
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import { TaskItem } from './components/TodoTask/TaskItem';
 import { todos } from './mock';
 
 export const App: React.FC = () => {
-  return (
+
+    const [todoList, setTodoList] = useState(todos);
+    const [newTaskName, setNewTaskName] = useState('');
+    const [newImg, setNewImg] = useState('');
+
+    const handleTodoDoneClick = (id: number) => {
+        const newTodoList = todoList.map(item => 
+            item.id === id 
+            ? {
+            ...item,
+            completed: true
+              }
+            :item
+        );
+        setTodoList(newTodoList);
+
+    }
+    const handleDeleteClick = (id: number) => {
+        const newTodoList = (todoList.filter((item) => item.id !== id));
+        setTodoList(newTodoList);
+    }
+
+    const AddClick = () => {
+        const newTodoList = [...todoList, {
+            userId: 100,
+            id: todoList.length + 1,
+            title: newTaskName,
+            completed: false,
+            url: newImg,
+        }];
+        setTodoList(newTodoList);
+        setNewTaskName("");
+        setNewImg("");
+
+    }
+    return (
       <>
           <h2>My tasks</h2>
+          <div>
+            <input 
+                type="text" 
+                value={newTaskName}
+                placeholder='New task' 
+                onChange={(e) => setNewTaskName(e.target.value)} />
+            <input 
+                type="text" 
+                value={newImg}
+                placeholder='URL Image ' 
+                onChange={(e) => setNewImg(e.target.value)} />
+            
+            <button title='button' onClick={AddClick}>Add</button>
+          </div>
           <ol>
-              {todos.map((item) => (
-                  <TaskItem item={item} key={item.id} />
+              {todoList.map((item) => (
+                  <TaskItem 
+                  item={item} 
+                  key={item.id} 
+                  DoneClick={() => handleTodoDoneClick(item.id)} 
+                  DeleteClick={() => handleDeleteClick(item.id)}/>
               ))}
           </ol>
       </>
-  );
+    );
 };
--- daru-winter-vite/src/components/TodoTask/TaskItem.tsx
+++ daru-winter-vite/src/components/TodoTask/TaskItem.tsx
@@ -4,18 +4,28 @@ import { PhotoItem } from './PhotoItem';
 
 type TaskItemProps = {
     item: TodoTask;
+    DoneClick: () => void;
+    DeleteClick: () => void;
 };
 
-export const TaskItem: React.FC<TaskItemProps> = ({ item }) => {
+
+
+export const TaskItem: React.FC<TaskItemProps> = ({ item, DoneClick, DeleteClick }) => {
     const { completed, title } = item;
 
     const status = completed ? 'Done' : 'Todo';
+
+    const handleDoneClick = () => {
+        DoneClick();
+    }
 
     return (
         <li>
             <h3>{title}</h3>
             <div>Status: {status}</div>
             <PhotoItem item={item}   />
+            {!completed && <button onClick={handleDoneClick}> Mark Is Done </button>}
+            <button onClick={() => DeleteClick()}>Delete</button>
         </li>
     );
 };
